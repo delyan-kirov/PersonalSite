d9f73d6cf79612c17986f7066834fefe7b50e3d8
delyan-kirov <delyan.k.kirov@gmail.com>
2023-12-24 00:46:46 +0200

    Adding some css and html to blog page

--- src/components/BlogPosts/BlogPosts.tsx
+++ src/components/BlogPosts/BlogPosts.tsx
@@ -1,24 +1,38 @@
 import React, { useRef } from "react";
 import "./BlogPosts.css";
+import TopBar from '../TopBar/TopBar';
 
 function BlogPosts() {
-  return (
-    <div style={{ fontSize: 24, margin: '1%' }}>
-      <a>Hello from BlogPosts!!!</a>
-      <div>
-        <p>
-          <a title="blog" href="/PersonalSite/#/post1">
-            Blog
-          </a>
-        </p>
-        <p>
-          <a title="blog" href="/PersonalSite/#/post1">
-            Blog
-          </a>
-        </p>
-      </div>
-    </div>
-  );
+    return (
+        <div className="BlogPosts">
+            <div className="TopBar">
+                <TopBar />
+            </div>
+            <div className="Title">
+                <a>Hello from BlogPosts!!!</a>
+            </div>
+            <div style={{ margin: "25px" }}></div>
+            <div className="BlogList">
+                <div className="BlogEntrie" onClick={() => { window.location.href = "/PersonalSite/#/post1"; }}>
+                    <p>
+                        <a title="blog">
+                            Blog
+                        </a>
+                    </p>
+                    <p>A short tutorial about Haskell data</p>
+                </div>
+            <div style={{ margin: "25px" }}></div>
+                <div className="BlogEntrie" onClick={() => { window.location.href = "/PersonalSite/#/post1"; }}>
+                    <p>
+                        <a title="blog">
+                            Blog
+                        </a>
+                    </p>
+                    <p> Something else </p>
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default BlogPosts;
