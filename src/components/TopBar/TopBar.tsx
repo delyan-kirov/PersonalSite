6fce448e9661d770eea5394bdb3a2e85238949f5
delyan-kirov <delyan.k.kirov@gmail.com>
2024-01-02 20:20:12 +0200

    Adding links inside the topbar

--- src/components/TopBar/TopBar.tsx
+++ src/components/TopBar/TopBar.tsx
@@ -3,23 +3,23 @@ import './TopBar.css';
 import resume from './Masters_Thesis.pdf';
 
 function TopBar() {
-  const handleEmailClick = () => {
-    const emailAddress = 'delyan.k.kirov@gmail.com'
-    // Generate the mailto link
-    const mailtoLink = `mailto:${emailAddress}?subject=${encodeURIComponent("")}&body=${encodeURIComponent("")}`;
+    const handleEmailClick = () => {
+        const emailAddress = 'delyan.k.kirov@gmail.com'
+        // Generate the mailto link
+        const mailtoLink = `mailto:${emailAddress}?subject=${encodeURIComponent("")}&body=${encodeURIComponent("")}`;
 
-    // Open the default email client with the pre-filled details
-    window.location.href = mailtoLink;
-  };
-  return (
-    <div className="topnav">
-      <div className='socials'>
-        <a>Projects</a>
-        <a>About</a>
-        <a>Blog</a>
-      </div>
-    </div>
-  );
+        // Open the default email client with the pre-filled details
+        window.location.href = mailtoLink;
+    };
+    return (
+        <div className="topnav">
+            <div className='socials'>
+                <a>Projects</a>
+                <a title="blog" href="/PersonalSite/">About</a>
+                <a title="blog" href="/PersonalSite/#/blog">Blog</a>
+            </div>
+        </div>
+    );
 }
 
 export default TopBar;
